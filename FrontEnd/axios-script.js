0d59fbccfcaa81c815977ee4c7cd10ade5d8c1b3
SON <wjdgh2777@naver.com>
2022-09-17 22:21:17 +0900

    Edit: 주소변경

--- FrontEnd/axios-script.js
+++ FrontEnd/axios-script.js
@@ -42,7 +42,7 @@ async function getDataSet(category) {
 
     const dataSet = await axios({
         method: "get", // http method
-        url: `http://localhost:3000/restaurants?category=${qs}`,
+        url: `http://43.200.140.142:3000/restaurants?category=${qs}`,
         headers: {}, // packet header
         data: {}, // packet body
     });
